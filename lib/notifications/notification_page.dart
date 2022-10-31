6ff689360771b61ae51305ba7224540de519f36b
nandhakumar-c <nandha.k@codingmart.com>
2022-10-31 15:45:15 +0530

    oct 31 commit

--- lib/notifications/notification_page.dart
+++ lib/notifications/notification_page.dart
@@ -1,20 +1,29 @@
 import 'package:bookmyshow/main.dart';
+import 'package:bookmyshow/provider/notification_provider.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:provider/provider.dart';
 
 class NotificationPage extends StatefulWidget {
-  const NotificationPage({Key? key}) : super(key: key);
+  const NotificationPage();
 
   @override
   State<NotificationPage> createState() => _NotificationPageState();
 }
 
 class _NotificationPageState extends State<NotificationPage> {
+  dynamic x;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    final notify = Provider.of<NotificationList>(context, listen: false);
+
+    // final notify = Provider.of<NotificationList>(context);
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       RemoteNotification? notification = message.notification;
       AndroidNotification? android = message.notification?.android;
@@ -29,6 +38,14 @@ class _NotificationPageState extends State<NotificationPage> {
       );
     });
 
+    OneSignal.shared.setNotificationOpenedHandler((data) {
+      notify.addNotificationDetails(
+          id: data.notification.notificationId,
+          title: data.notification.title!,
+          body: data.notification.body!,
+          imgurl: data.notification.bigPicture!);
+    });
+
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print('onMessageOpenedApp event published');
       RemoteNotification? notification = message.notification;
@@ -39,7 +56,7 @@ class _NotificationPageState extends State<NotificationPage> {
             return AlertDialog(
               title: Text(
                 '${notification?.title}',
-                style: TextStyle(fontWeight: FontWeight.w700),
+                style: const TextStyle(fontWeight: FontWeight.w700),
               ),
               content: SingleChildScrollView(
                   child: Column(
@@ -64,25 +81,72 @@ class _NotificationPageState extends State<NotificationPage> {
                 icon: '@mipmap/ic_launcher')));
   }
 
+  void addNotification(NotificationList notify) {
+    notify.addNotificationDetails(
+        id: "1", title: "Hi", body: "Test12", imgurl: "hello");
+  }
+
   @override
   Widget build(BuildContext context) {
+    final notify = Provider.of<NotificationList>(context);
+    OneSignal.shared.setNotificationOpenedHandler((data) {
+      notify.addNotificationDetails(
+          id: data.notification.notificationId,
+          title: data.notification.title!,
+          body: data.notification.body!,
+          imgurl: data.notification.bigPicture!);
+    });
+    // notify.addNotificationDetails();
     return Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-                icon: Icon(Icons.chevron_left),
-                onPressed: () => Navigator.of(context).pop()),
-            backgroundColor: Color.fromARGB(255, 10, 21, 46),
-            actions: [
-              IconButton(
-                onPressed: showNotification,
-                icon: Icon(Icons.notification_add),
-              )
-            ],
-            title: Text("Notifications")),
-        body: Center(
-          child: Container(
-            child: Text('No New Notifications'),
-          ),
-        ));
+      appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.chevron_left),
+              onPressed: () => Navigator.of(context).pop()),
+          backgroundColor: Color.fromARGB(255, 10, 21, 46),
+          actions: [
+            IconButton(
+              onPressed: () => addNotification(notify),
+              icon: Icon(Icons.notification_add),
+            )
+          ],
+          title: Text("Notifications")),
+      body: notify.notificationsList.isEmpty
+          ? Center(child: Text("No New Notifications"))
+          : SingleChildScrollView(
+              child: Column(children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const ScrollPhysics(),
+                  itemCount: notify.notificationsList.length,
+                  itemBuilder: (context, index) => Container(
+                    child: Dismissible(
+                      direction: DismissDirection.horizontal,
+                      onDismissed: ((direction) => notify.deleteNotification(
+                          notify.notificationsList[index].id)),
+                      key: ValueKey(notify.notificationsList[index].id),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                  "${notify.notificationsList[index].title}"),
+                              subtitle: Text(
+                                  "${notify.notificationsList[index].body}"),
+                            ),
+                            FittedBox(
+                              fit: BoxFit.cover,
+                              child: Image.network(
+                                  notify.notificationsList[index].imgurl),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+    );
   }
 }
